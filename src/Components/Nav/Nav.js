82e7333ca5d05ff8395424c2aecd4060fe6bedc3
Jaden Goodwin <jaden.goodwin24@gmail.com>
2017-11-21 22:38:53 -0700

    nav bar mobile responsive except need to close if widened

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -7,15 +7,28 @@ import gdLogoWhite from '../Assets/gdLogoWhite.png';
 import ProjectIcon from '../Assets/codeIconWhite.png';
 import ContactIcon from '../Assets/contactWhite.png';
 import AboutIcon from '../Assets/aboutWhite.png';
-import SkillsIcon from '../Assets/skillsWhite1.png';
+import SkillsIcon from '../Assets/skillWhite.png';
 
 // social icons 
 import fbLogo from '../Assets/facebookWhite.png';
 import LinLogo from '../Assets/linkedInWhite.png';
 import pinLogo from '../Assets/pinterestWhite.png';
 import insLogo from '../Assets/instagramWhite.png';
-import twiLogo from '../Assets/twitterWhite.png';    
+import twiLogo from '../Assets/twitterWhite.png';
 class Nav extends Component {
+    constructor() {
+        super();
+        this.state = {
+            toggleMenu: false
+        }
+
+        this.toggleMenuFunc = this.toggleMenuFunc.bind(this);
+    }
+    toggleMenuFunc() {
+        this.setState({
+            toggleMenu: !this.state.toggleMenu
+        })
+    }
     render() {
         return (
             <div className='NavHolder'>
@@ -35,15 +48,47 @@ class Nav extends Component {
                         <Link to={'/contact'}><div className='imgLinkHolder'><img src={ContactIcon} /><p className='linkName'>CONTACT</p></div></Link>
                     </div>
 
+
                     <div className='navSection rightNav'>
                         <img src={fbLogo} alt='logo' />
                         <img src={LinLogo} alt='logo' />
                         <img src={twiLogo} alt='logo' />
                         <img src={insLogo} alt='logo' />
-                        
-                        
+                    </div>
+                    {/* drop down menu button */}
+                    <div className='dropDownMenuBtn' onClick={this.toggleMenuFunc}>
+                        <div className={this.state.toggleMenu ? 'bar bar1 animateBar1' : 'bar bar1'}></div>
+                        <div className={this.state.toggleMenu ? 'bar midBar1 animateMidBar1' : 'bar midBar1'}></div>
+                        <div className={this.state.toggleMenu ? 'bar midBar2 animateMidBar2' : 'bar midBar2'}></div>
+                        <div className={this.state.toggleMenu ? 'bar bar3 animateBar3' : 'bar bar3'}></div>
+
                     </div>
                 </div>
+                 
+                {/*===| DROP DOWN MENU |=================================*/}
+                <div className={this.state.toggleMenu ? 'dropDownMenu displayMenu' : 'dropDownMenu hideMenu'}>
+                    {/* menuSectionHolder*/}
+                    <div className='menuSectionHolder'>
+                        {/* section page links  */}
+                        <div className='menuPageLinksHolder'>
+                            <Link to={'/about'}><p className='menuPageLink'>ABOUT</p></Link>
+                            <Link to={'/projects'}><p className='menuPageLink'>PROJECTS</p></Link>
+                            <Link to={'/skills'}><p className='menuPageLink'>SKILLS</p></Link>
+                            <Link to={'/contact'}><p className='menuPageLink'>CONTACT</p></Link>
+                        </div>
+
+                        {/* section - social links */}
+                        <div className='menuSocialLinksHolder'>
+                            <img src={fbLogo} alt='logo' />
+                            <img src={LinLogo} alt='logo' />
+                            <img src={twiLogo} alt='logo' />
+                            <img src={insLogo} alt='logo' />
+                        </div>
+
+                    </div>{/* end of menuSectionHolder */}
+
+                </div>
+
             </div>
         )
     }
