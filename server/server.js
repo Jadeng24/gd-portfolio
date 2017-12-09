ffed06cf9f606ab650130aca43c266fbcf085877
Jaden Goodwin <jaden.goodwin24@gmail.com>
2017-12-08 22:47:30 -0700

    build pointer

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const express = require('express'),
 const app = express();
 
 app.use(cors());
-
+app.use(express.static(`${__dirname}/../build`));
 app.use(bodyParser.json());
 app.use(session({
     secret: process.env.SECRET,
